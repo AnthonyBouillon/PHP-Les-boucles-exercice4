4cbdfca7da3a8f846485aaa3397922cbab0bd28a
Anthony <anthony_bouillon@outlook.fr>
2017-12-12 15:07:45 +0100

    Les boucles

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Les boucles exercice 4</title>
+  </head>
+  <body>
+    <?php
+$number = 1;
+while($number < 10){
+  echo '<p>' .$number. '</p>';
+  $number = $number + 0.5;
+}
+     ?>
+  </body>
+</html>
